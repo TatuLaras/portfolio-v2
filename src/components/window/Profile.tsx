693a91401cbea82d2d751edc30bf79a2fa52d9a3
Tatu Laras <tatu.laras@gmail.com>
2024-05-19 14:19:15 +0300

    Profile picture

--- src/components/window/Profile.tsx
+++ src/components/window/Profile.tsx
@@ -6,13 +6,13 @@ type Props = { delayValue: number };
 export default function Profile({ delayValue }: Props) {
     return (
         <>
-            {/* <div
-                className='face basic-panel right animate-open'
+            <div
+                className="face basic-panel right animate-open"
                 style={delay(delayValue)}
             >
-                <div className='barcode'>lvgssig</div>
-            </div> */}
-            <div className='jitter profile' style={delay(delayValue)}>
+                <div className="barcode">lvgssig</div>
+            </div>
+            <div className="jitter profile" style={delay(delayValue)}>
                 <p>
                     {/* <TextTypeAnimation
                         timePerChar={3}
