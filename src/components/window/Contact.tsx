c3ff8829d26d0e1c7a8a7f6baba65b1f77d9a1db
Tatu Laras <tatu.laras@gmail.com>
2024-12-15 16:33:44 +0200

    Updated content

--- src/components/window/Contact.tsx
+++ src/components/window/Contact.tsx
@@ -1,24 +1,19 @@
 export default function Contact() {
     return (
-        <div className='contact'>
+        <div className="contact">
             <p>Yhteystiedot yms:</p>
             <p>
-                <a href='mailto:tatu.laras@gmail.com' target='_blank'>
+                <a href="mailto:tatu.laras@gmail.com" target="_blank">
                     tatu.laras@gmail.com
                 </a>
             </p>
             <p>
-                <a href='tel:+358451787499' target='_blank'>
-                    +358 45 1787499
-                </a>
-            </p>
-            <p>
-                <a href='http://www.linkedin.com/in/tatularas' target='_blank'>
+                <a href="http://www.linkedin.com/in/tatularas" target="_blank">
                     LinkedIn
                 </a>
             </p>
             <p>
-                <a href='https://github.com/TatuLaras/' target='_blank'>
+                <a href="https://github.com/TatuLaras/" target="_blank">
                     GitHub
                 </a>
             </p>
